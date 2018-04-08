ddc35635c0a6f8f8b381800f5015abf89f097eb0
bradszalach <37637591+bradszalach@users.noreply.github.com>
2018-04-08 15:05:32 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+#Chef Selector - Are you a chef?
+#Coded By Brad Szalach
+
+import random
+
+operation = input("-How many yeezys can you secure? Enter a number: ")
+if(operation == '0'):
+	print(input("U are no chef, u are certified washed \nPress enter to see how many shoes you will cook the 14th: "))
+	print(random.randint(1,5))
+elif(operation == '1'):
+	print(input("U are no chef, u are certified washed \nPress enter to see how many shoes you will cook the 14th: "))
+	print(random.randint(1,5))
+elif(operation == '2'):
+	print(input("U are no chef, u are certified washed \nPress enter to see how many shoes you will cook the 14th: "))
+	print(random.randint(1,5))
+elif(operation == '3'):
+	print(input("U are no chef, u are certified washed \nPress enter to see how many shoes you will cook the 14th: "))
+	print(random.randint(1,5))
+elif(operation == '4'):
+	print(input("U are no chef, u are certified washed \nPress enter to see how many shoes you will cook the 14th: "))
+	print(random.randint(1,5))
+elif(operation == '5'):
+	print(input("U are no chef, u are certified washed \nPress enter to see how many shoes you will cook the 14th: "))
+	print(random.randint(1,5))
+	
+else:
+	operation2 = input("-Okay you passed the first test \n-How many off whites can you cook? Enter a number: ")
+	if(operation2 == '0'):
+		print(input("U a semi-chef, but not fully washed \nPress enter to see how many shoes you will cook the 14th: "))
+		print(random.randint(1,10))
+	elif(operation2 == '1'):
+		print(input("U a semi-chef, but not fully washed \nPress enter to see how many shoes you will cook the 14th: ")) 
+		print(random.randint(1,10))
+	elif(operation2 == '2'):
+		print(input("U a semi-chef, but not fully washed \nPress enter to see how many shoes you will cook the 14th: "))
+		print(random.randint(1,10))
+	elif(operation2 == '3'):
+		print(input("U a semi-chef, but not fully washed \nPress enter to see how many shoes you will cook the 14th: "))
+		print(random.randint(1,10))
+	elif(operation2 == '4'):
+		print(input("U a semi-chef, but not fully washed \nPress enter to see how many shoes you will cook the 14th: "))
+		print(random.randint(1,10))
+	elif(operation2 == '5'):
+		print(input("U a semi-chef, but not fully washed \nPress enter to see how many shoes you will cook the 14th: "))
+		print(random.randint(1,10))
+	elif(operation != 0, 1, 2, 3, 4, 5):
+		operation3 = input("-Congrats u are certified chef \n-Now tell me...are you in any of these groups? \nA)KDG \nB)Shoeplex \nC)AM Notify \nD)AKChefs \nE)None of the above \nEnter A, B, C, D, or E: ")
+		if(operation3 == 'A'):
+			print(input("Congrats you are a chef and apart of the best cook group out there \nPress enter to see how many shoes you will cook the 14th: "))
+			print(random.randint(1,15))
+		elif(operation3 == 'B'):
+			print(input("Congrats you are a chef and part of a good cook group \nPress enter to see how many shoes you will cook the 14th: "))
+			print(random.randint(1,15))
+		elif(operation3 == 'C'):
+			print(input("Congrats you are a chef and probably hit many restocks \nPress enter to see how many shoes you will cook the 14th: "))
+			print(random.randint(1,15))
+		elif(operation3 == 'D'):
+			print(input("Congrats you are a chef and idk if this group is good or not \nPress enter to see how many shoes you will cook the 14th: "))
+			print(random.randint(1,15))
+		elif(operation3 == 'D'):
+			print(input("Congrats you are a chef all by yourself \nPress enter to see how many shoes you will cook the 14th: "))
+			print(random.randint(1,15))
+		
+
+
+
+	
